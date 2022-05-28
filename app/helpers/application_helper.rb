a0413328d6d9561864babc0a48733f21d25c325a
psmishenko <psmishenko@gmail.com>
2022-05-28 14:42:42 +0300

    Add Md format method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def md_format(text)
+    options = { no_intra_emphasis: true, fenced_code_blocks: true, autolink: true, strikethrough: true,
+                hard_wrap: true, disable_indented_code_blocks: true }
+    Markdown.new(text, options).to_html
+  end
 end
